01c6b4425da53f10044f1e964facaa1ceb59a829
ndt0409 <vivucenter@gmail.com>
2022-01-12 10:23:43 +0700

    Setup Firebase Cloud Messaging & Firestore Database

--- app/src/main/java/com/ndt/tchatapp/activities/SignInActivity.kt
+++ app/src/main/java/com/ndt/tchatapp/activities/SignInActivity.kt
@@ -3,7 +3,8 @@ package com.ndt.tchatapp.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.ndt.tchatapp.R
+import android.widget.Toast
+import com.google.firebase.firestore.FirebaseFirestore
 import com.ndt.tchatapp.databinding.ActivitySignInBinding
 
 class SignInActivity : AppCompatActivity() {
@@ -15,9 +16,26 @@ class SignInActivity : AppCompatActivity() {
         setListeners()
     }
 
-    fun setListeners() {
+    private fun setListeners() {
         binding.tvCreate.setOnClickListener {
             startActivity(Intent(this, SignUpActivity::class.java))
         }
+        binding.btnSignIn.setOnClickListener {
+            //addDataToFireStore()
+        }
     }
+
+//    private fun addDataToFireStore() {
+//        val hashMap = HashMap<String, Any>()
+//        hashMap["first_name"] = "Nguyen"
+//        hashMap["last_name"] = "Tu"
+//        FirebaseFirestore.getInstance().collection("users")
+//            .add(hashMap)
+//            .addOnSuccessListener {
+//                Toast.makeText(this, "Data insert", Toast.LENGTH_SHORT).show()
+//
+//            }.addOnFailureListener {
+//                Toast.makeText(this, "Data insert", Toast.LENGTH_SHORT).show()
+//            }
+//    }
 }
